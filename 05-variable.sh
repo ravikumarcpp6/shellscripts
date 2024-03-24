3884ef801cb7dfeb819e225bd2589c2e9121169f
ravikumarcpp6 <ravikumarcpp6@gmail.com>
2024-03-24 15:58:02 +0530

    first shell script

--- 05-variable.sh
+++ 05-variable.sh
@@ -3,6 +3,6 @@
 echo "please enter username"
 read USERNAME #username taken from screen
 echo "please enter password"
-read PASSWORD #password taken from screen
+read -s PASSWORD #password taken from screen
 echo "Username is : $USERNAME"
 echo "Password is : $PASSWORD"
