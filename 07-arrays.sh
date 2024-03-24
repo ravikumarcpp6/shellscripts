a1eef20872f0b71baf42451c25b4060e3469321a
ravikumarcpp6 <ravikumarcpp6@gmail.com>
2024-03-24 16:19:04 +0530

    first shell script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -5,3 +5,5 @@ FRUIT=("APPLE" "BANANA" "ROSES")
 echo "the first fruit is: ${FRUIT[0]}"
 echo "the second fruit is : ${FRUIT[1]}"
 echo "the third fruit is : ${FRUIT[2]}"
+
+echo "all fruits are : ${FRUIT[@]}"
