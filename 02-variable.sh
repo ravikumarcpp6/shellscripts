0e6c94db0563d16f76f635b7a6ba4fd2317204fa
ravikumarcpp6 <ravikumarcpp6@gmail.com>
2024-03-24 15:34:49 +0530

    first shell script

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 
-PERSON1=Ram
-PERSON2=Raheem
-echo "$PERSON1: Hello $PERSON2,Good Morning"
-echo "$PERSON2: Hi $PERSON1,Very Good Morning"
-echo "$PERSON1: How are you doing $PERSON2"
-echo "$PERSON2: I am Doing Great $PERSON1,How are you"
