54b7da81f9294a216ac73c25d6413550be2ac1f7
ravikumarcpp6 <ravikumarcpp6@gmail.com>
2024-03-24 14:49:18 +0530

    first shell script

---
+++ 02-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hello Suresh,Good Morning"
+echo "Suresh: Hi Ramesh,Very Good Morning"
+echo "Ramesh: How are you doing Suresh"
+echo "Suresh: I am Doing Great Ramesh,How are you"
