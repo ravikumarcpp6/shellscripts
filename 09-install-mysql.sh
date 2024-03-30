ee93239fedbe64b5b76a3e62d1e6f15242a7ee27
ravikumarcpp6 <ravikumarcpp6@gmail.com>
2024-03-30 13:17:36 +0530

    new shell

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -5,6 +5,7 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
     then 
         echo "ERROR:: Run Script with root user"
+        exit 1 # give other than zero to stop
     else
         echo "You Are ROOT USER"
-fi
+fi  # fi means reverse of if, indicating condition end
