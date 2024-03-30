5178f16a71a44540bd2df401d697c26c206685eb
ravikumarcpp6 <ravikumarcpp6@gmail.com>
2024-03-30 15:35:39 +0530

    new shell

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -4,3 +4,7 @@ NUMBER1=$1
 NUMBER2=$2
 SUM=$((NUMBER1+NUMBER2))
 echo "the sum is : $SUM"
+
+echo  "how many arguments passed:: $#"
+echo "what are the all arguments passed:: $@"
+echo "scritp same is $0"
