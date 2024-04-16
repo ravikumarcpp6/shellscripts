1080c6aba8d99a0da0bf37ac3224cab3ab2f953b
ravikumarcpp6 <ravikumarcpp6@gmail.com>
2024-04-16 13:03:40 +0530

    shell script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -11,7 +11,9 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
 
-echo "All Arguments Passed: $@"
+#echo "All Arguments Passed: $@"
+#git mysql postfix net-tools
+#package=git for first time
 
 if [ $ID -ne 0 ]
  then 
@@ -21,4 +23,28 @@ if [ $ID -ne 0 ]
      echo -e "$G You are a ROOT user $N"    
 fi    
 
+VALIDATE
+         { 
+            if [ $1 -ne 0 ]
+            then echo -e "$2...$R FAILED $N"
+                          exit 1
+            else 
+                 echo -e "$2...$ GSuccess $N"       
+            fi            
+
+           }
+
 echo "TIME IS ::$TIMESTAMP"
+
+for package in $@
+do 
+    yum list installed $package
+        if[ $? -ne 0]
+        then 
+         yum install $package -y & >> $LOGFILE
+         VALIDATE $? "Installing $package" 
+         else 
+          echo -e "$package has already installed $Y SKIPPING...$N"
+
+
+done
